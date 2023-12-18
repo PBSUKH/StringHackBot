c777c7e5142fc75592e49b24a51d6a0709d51823
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2023-12-18 21:27:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 class Config:
     API_ID = "22403973"
     API_HASH = "2dcff71afb07f9a54fe33af0113f1da6"
-    TOKEN = "6481216235:AAHIsr9n3yTpnOL6cybmh-jgMotTwUS7bUI"
+    TOKEN = "6481216235:AAFOO7ordqQtm4GbewwiCf5Vhx_sGViO5ZA"
     MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
     SUDOERS = filters.user(["6085298975"])
