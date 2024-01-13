320d8f5dde14d0cc10dcf8effa1f358e2b6564e1
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-13 22:47:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     API_HASH = "3a0d920a6370b97671ea838d92dc559c"
     TOKEN = "6583382553:AAHmsFGuN6QrR2e_mLRvu66HaVhNSSMLYGY"
     MONGO_URL = "mongodb+srv://Badmunda_13:badmunda50@cluster0.9oyzqux.mongodb.net/?retryWrites=true&w=majority"
-    START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
+    START_PIC = "https://telegra.ph/file/b5d3cd24544102a4c454f.jpg"
     SUDOERS = filters.user(["6566179661"])
